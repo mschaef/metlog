bc56482991cc617e538fc99c9c3f379f4fea94a9
Mike Schaeffer <mike@mschaef.com>
2015-05-26 23:10:06 -0400

    Update agent to make more 'production-ready'.

--- metlog-agent/src/metlog_agent/core.clj
+++ metlog-agent/src/metlog_agent/core.clj
@@ -9,26 +9,46 @@
 
 (def start-t (System/currentTimeMillis))
 
-(defn poll-sensor-1 []
+(def sensor-result-queue (java.util.concurrent.LinkedBlockingQueue.))
+
+(defn sensor-reading [ series-name val ]
   {:t (java.util.Date.)
-   :series_name "sine"
-   :val (Math/sin (/ (- (System/currentTimeMillis) start-t) 250.0))})
+   :series_name series-name
+   :val val})
+
+(defn enqueue-sensor-reading [ reading ]
+  (log/info "enquing sensor reading" reading)
+  (.add sensor-result-queue reading))
+
+(defn poll-sensor-1 []
+  (enqueue-sensor-reading
+   (sensor-reading "sine" (Math/sin (/ (- (System/currentTimeMillis) start-t) 60000.0)))))
 
 (defn poll-sensor-2 []
-  {:t (java.util.Date.)
-   :series_name "cosine"
-   :val (Math/cos (/ (- (System/currentTimeMillis) start-t) 250.0))})
+  (enqueue-sensor-reading
+   (sensor-reading "cosine" (Math/cos (/ (- (System/currentTimeMillis) start-t) 60000.0)))))
 
-(def sensor-result-queue (java.util.concurrent.LinkedBlockingQueue.))
+(defn poll-w1-sensor [ path]
+  (with-open [rdr (clojure.java.io/reader path)]
+  (let [ line (second (line-seq rdr)) ]
+    (if (not (nil? line))
+      (enqueue-sensor-reading
+       (sensor-reading "basement-temp"
+        (/ (Double/valueOf (.substring line 29)) 1000.0)))))))
 
 (defn poll-sensors []
   (log/debug "poll-sensors")
   (locking sensor-result-queue
-    (.add sensor-result-queue (poll-sensor-1))
-    (.add sensor-result-queue (poll-sensor-2))))
+    (poll-sensor-1)
+    (poll-sensor-2)
+    (if-let [ sensor-path (config-property "sensor.path") ]
+      (poll-w1-sensor sensor-path))))
 
 (defn do-update [ snapshot ]
-  (client/post "http://localhost:8080/data" { :body (pr-str snapshot)}))
+  (let [url (config-property "vault.url" "http://localhost:8080/data")
+        data { :body (pr-str snapshot)}]
+    (log/info "Posting" data " to " url)
+    (client/post url data)))
 
 (defn update-vault []
   (log/debug "update-vault")
@@ -40,8 +60,8 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (at-at/every 1000 (exception-barrier poll-sensors) my-pool)
-  (at-at/every 5000 (exception-barrier update-vault) my-pool)
+  (at-at/every 15000 (exception-barrier poll-sensors) my-pool)
+  (at-at/every 60000 (exception-barrier update-vault) my-pool)
   (println "Running."))
 
 
