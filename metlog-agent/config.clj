924bc78ec04978783ee2001ba0ce1c86d483c8f8
Mike Schaeffer <michael.schaeffer@lab49.com>
2015-10-18 10:22:16 -0400

    Workaround type coersion issue in backend by using explicit double scaling constants in config.clj.

--- metlog-agent/config.clj
+++ metlog-agent/config.clj
@@ -1,11 +1,11 @@
 (def start-t (System/currentTimeMillis))
 
 (defsensor sine {:poll-interval (seconds 10)}
-  (Math/sin (/ (- (System/currentTimeMillis) start-t) (minutes 1))))
+  (Math/sin (/ (- (System/currentTimeMillis) start-t) 60000.0)))
 
 (defsensor cosine {:poll-interval (seconds 10)}
-  (Math/cos (/ (- (System/currentTimeMillis) start-t) (minutes 1))))
+  (Math/cos (/ (- (System/currentTimeMillis) start-t) 60000.0)))
 
 (defsensor steps-ascending {:poll-interval (seconds 5)}
-  (mod (/ (- (System/currentTimeMillis) start-t) (minutes 2))
+  (mod (/ (- (System/currentTimeMillis) start-t) 120000.0)
        3))
