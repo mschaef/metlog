77e54b013b7cba30137d6f7104483d321373da29
Mike Schaeffer <michael.schaeffer@lab49.com>
2015-10-18 10:09:42 -0400

    Remove the cosine-2 test series and replace with a sawtooth series for identiying sign issues.

--- metlog-agent/config.clj
+++ metlog-agent/config.clj
@@ -1,10 +1,11 @@
 (def start-t (System/currentTimeMillis))
 
 (defsensor sine {:poll-interval (seconds 10)}
-  (Math/sin (/ (- (System/currentTimeMillis) start-t) 60000.0)))
+  (Math/sin (/ (- (System/currentTimeMillis) start-t) (minutes 1))))
 
 (defsensor cosine {:poll-interval (seconds 10)}
-  (Math/cos (/ (- (System/currentTimeMillis) start-t) 60000.0)))
+  (Math/cos (/ (- (System/currentTimeMillis) start-t) (minutes 1))))
 
-(defsensor cosine-2 {:poll-interval (seconds 5)}
-  (Math/cos (/ (- (System/currentTimeMillis) start-t) 30000.0)))
+(defsensor steps-ascending {:poll-interval (seconds 5)}
+  (mod (/ (- (System/currentTimeMillis) start-t) (minutes 2))
+       3))
