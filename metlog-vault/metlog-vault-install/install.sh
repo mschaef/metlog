bfb593ee063e6d4531df45109de4bd01adf49652
Mike Schaeffer <mike@mschaef.com>
2016-12-21 16:15:29 -0500

    Add an install script for the agent, and use separate service names for the agent and vault.

---
+++ metlog-vault/metlog-vault-install/install.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+METLOG_SERVICE_NAME=metlog-vault
+METLOG_USER_NAME=${METLOG_SERVICE_NAME}
+
+# Must be root
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# create user and group
+
+egrep "^${METLOG_USER_NAME}" /etc/passwd >/dev/null
+
+if [ $? -eq 0 ]; then
+    echo "User exists: ${METLOG_USER_NAME}"
+else
+    echo "Creating user: ${METLOG_USER_NAME}"
+    
+    useradd --user-group --system ${METLOG_USER_NAME}
+
+    if [ $? -ne 0 ]; then
+        echo "Could not create user: ${METLOG_USER_NAME}"
+        exit 1
+    fi
+fi
+
+# Install jar in /usr/share
+
+install -v --group=root --owner=root --directory /usr/share/${METLOG_SERVICE_NAME}
+install -v --group=root --owner=root ${METLOG_SERVICE_NAME}-standalone.jar /usr/share/${METLOG_SERVICE_NAME}
+
+# create log directory
+
+install -v --group=${METLOG_SERVICE_NAME} --owner=${METLOG_SERVICE_NAME} --directory /var/log/${METLOG_SERVICE_NAME}
+
+# create data directory
+
+install -v --group=${METLOG_SERVICE_NAME} --owner=${METLOG_SERVICE_NAME} --directory /var/lib/${METLOG_SERVICE_NAME}
+
+# Configuration Files
+
+install -v --group=root --owner=root --directory /etc/${METLOG_SERVICE_NAME}
+install -v --group=root --owner=root logback.xml /etc/${METLOG_SERVICE_NAME}
+
+# metlog service configuration
+
+install -v --group=root --owner=root ${METLOG_SERVICE_NAME} /etc/init.d
+
+update-rc.d ${METLOG_SERVICE_NAME} defaults
+
