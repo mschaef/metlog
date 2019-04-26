e1b73791137e594ef0b0f8104a3d90ca3cf4b217
Mike Schaeffer <mike@mschaef.com>
2019-04-26 19:35:59 -0400

    Version 0.1.0

--- metlog-agent/project.clj
+++ metlog-agent/project.clj
@@ -1,4 +1,4 @@
-(defproject metlog-agent "0.1.0-SNAPSHOT"
+(defproject metlog-agent "0.1.0"
   :description "Metlog agent - polls data sources for upload to vault."
   
   :license {:name "Eclipse Public License"
