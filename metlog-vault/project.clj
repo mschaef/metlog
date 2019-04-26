b908f15ce561e932f1a56341f0614532f3818afa
Mike Schaeffer <mike@mschaef.com>
2019-04-26 19:33:45 -0400

    Version 0.3.0-SNAPSHOT

--- metlog-vault/project.clj
+++ metlog-vault/project.clj
@@ -1,4 +1,4 @@
-(defproject metlog-vault "0.2.2"
+(defproject metlog-vault "0.3.0-SNAPSHOT"
   :description "Repository for long term storage of series data."
   
   :license {:name "Eclipse Public License"
