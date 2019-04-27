fcf0e33531ae7001ad58da36660b2f1a6f247750
Mike Schaeffer <mike@mschaef.com>
2019-04-27 10:59:24 -0400

    Remove the schema v0 def from the v1, now that sql-file can properly handle it.

--- metlog-vault/resources/schema-metlog-1.sql
+++ metlog-vault/resources/schema-metlog-1.sql
@@ -1,24 +1,3 @@
--- version 0
-
-CREATE CACHED TABLE series (
-       series_id INTEGER IDENTITY,
-       series_name VARCHAR(128) NOT NULL
-);
-
-CREATE CACHED TABLE sample (
-       series_id INTEGER NOT NULL REFERENCES series(series_id),       
-       t TIMESTAMP NOT NULL,
-       val DOUBLE NOT NULL
-);
-
-CREATE INDEX idx_sample_t ON sample(t);
-
-CREATE CACHED TABLE dual (
-       x INTEGER
-);
-
-INSERT INTO dual(x) values(1);
-
 -- version 1
 
 CREATE CACHED TABLE dashboard (
