663017a3ede759f79ee31aafb8584227b4ceed40
Mike Schaeffer <mike@mschaef.com>
2022-08-29 07:32:49 -0500

    Version 0.6.3

--- metlog-vault/project.clj
+++ metlog-vault/project.clj
@@ -1,4 +1,4 @@
-(defproject metlog-vault "0.6.3-SNAPSHOT"
+(defproject metlog-vault "0.6.3"
   :description "Repository for storing and displaying time series data reported by metlog-agent.."
 
   :license {:name "Eclipse Public License"
