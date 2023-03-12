3a0cfe2c8132aaad1d506281813098cdb4347311
Mike Schaeffer <mike@mschaef.com>
2023-03-11 22:09:27 -0500

    Version 0.8.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metlog "0.8.2"
+(defproject metlog "0.8.3-SNAPSHOT"
   :description "Lightweight tool for gathering, storing, and inspecting metrics."
 
   :license {:name "Eclipse Public License"
