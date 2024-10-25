037b08fc36c4d4593cc15c62910245fe10e76099
Mike Schaeffer <mike@mschaef.com>
2024-10-25 06:44:53 -0400

    Update dependencies.

--- project.clj
+++ project.clj
@@ -4,23 +4,23 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [overtone/at-at "1.2.0"]
-                 [com.mschaef/sql-file "0.4.8"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [overtone/at-at "1.4.65"]
+                 [com.mschaef/sql-file "0.4.11"]
                  [yesql "0.5.3"]
-                 [ring/ring-jetty-adapter "1.9.6"]
+                 [ring/ring-jetty-adapter "1.13.0"]
                  [slester/ring-browser-caching "0.1.1"]
-                 [ring/ring-devel "1.9.6"]
+                 [ring/ring-devel "1.13.0"]
                  [ring/ring-json "0.5.1"]
-                 [com.cognitect/transit-clj "1.0.329"]
-                 [compojure "1.7.0"
+                 [com.cognitect/transit-clj "1.0.333"]
+                 [compojure "1.7.1"
                   :exclusions [commons-codec]]
                  [hiccup "1.0.5"]
-                 [org.clojure/data.json "2.4.0"]
+                 [org.clojure/data.json "2.5.0"]
                  [clj-time "0.15.2"]
-                 [clj-http "3.12.3"
+                 [clj-http "3.13.0"
                   :exclusions [com.fasterxml.jackson.core/jackson-core]]
-                 [com.mschaef/playbook "0.1.3-SNAPSHOT"]]
+                 [com.mschaef/playbook "0.1.3"]]
 
   :plugins [[lein-tar "3.3.0"]]
 
