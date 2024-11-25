39236054823ce8fd21c22f6752cbaf84aa89fb24
Mike Schaeffer <mike@mschaef.com>
2024-11-25 13:20:46 -0500

    Update 19096 sensor definitions.

--- pkg/sensor-19096.clj
+++ pkg/sensor-19096.clj
@@ -1,6 +1,6 @@
-;; config.clj
+;; sensor-19096.clj
 ;;
-;; Custom configuration goes here
+;; Custom sensor definitions go here
 
 (defn constrain-sensor-range [ sensor-val low high ]
   (and sensor-val
@@ -28,36 +28,36 @@
   (read-w1-sensor-at-path "/sys/bus/w1/devices/28-0000068f415f/w1_slave"))
 
 (defsensor "phl-downstairs-humidity" {:poll-interval (minutes 1)}
-   (let [body (http-request-json "http://10.0.0.150/tstat/humidity")]
+   (let [body (http-request-json "http://192.168.1.160/tstat/humidity")]
      (and body
-        (ensure-number (get body "humidity" false)))))
+        (ensure-number (get body :humidity false)))))
 
 (defsensor "phl-upstairs-humidity" {:poll-interval (minutes 1)}
-   (let [body (http-request-json "http://10.0.0.151/tstat/humidity")]
+   (let [body (http-request-json "http://192.168.1.161/tstat/humidity")]
      (and body
-        (ensure-number (get body "humidity" false)))))
+        (ensure-number (get body :humidity false)))))
 
 (defn poll-ct-80 [ url ]
   (let [body (http-request-json url)]
     (and body
          (->
-          {:temp-f (constrain-sensor-range (ensure-number (get body "temp" false)) 20 120)
-           :tmode (ensure-number (get body "tmode" false))
-           :fmode (ensure-number (get body "fmode" false))
-           :cool-setpoint-f (or (ensure-number (get body "t_cool" false)) -1)
-           :heat-setpoint-f (or (ensure-number (get body "t_heat" false)) -1)}))))
+          {:temp-f (constrain-sensor-range (ensure-number (get body :temp false)) 20 120)
+           :tmode (ensure-number (get body :tmode false))
+           :fmode (ensure-number (get body :fmode false))
+           :cool-setpoint-f (or (ensure-number (get body :t_cool false)) -1)
+           :heat-setpoint-f (or (ensure-number (get body :t_heat false)) -1)}))))
 
 (defsensor "phl-downstairs" {:poll-interval (minutes 1)}
-  (poll-ct-80 "http://10.0.0.150/tstat"))
+  (poll-ct-80 "http://192.168.1.160/tstat"))
 
 (defsensor "phl-upstairs" {:poll-interval (minutes 1)}
-  (poll-ct-80 "http://10.0.0.151/tstat"))
+  (poll-ct-80 "http://192.168.1.161/tstat"))
 
 (defsensor "mschaef-main" {:poll-interval (minutes 30)}
-  (measure-http-get "http://www.mschaef.com"))
+  (measure-http-get "https://www.mschaef.com"))
 
 (defsensor "mschaef-rss" {:poll-interval (minutes 30)}
-  (measure-http-get "http://www.mschaef.com/feed/rss"))
+  (measure-http-get "https://www.mschaef.com/feed/rss"))
 
 (defsensor "bandwidth-test-19096" {:poll-interval (minutes 30)}
   (measure-http-get "https://s3.amazonaws.com/bandwidth-test.mschaef.com/10-mib"))
